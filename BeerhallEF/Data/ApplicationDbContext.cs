94e495d4d21e745ebe8009fbd683c52f429995ed
Karine Samyn <Karine.Samyn@hogent.be>
2019-10-05 16:24:07 +0200

    Add Class Brewer and Mapping
    - Created a Domain Class: Brewer
    - Created a Db Mapping BrewerConfiguration
    (used to map the Domain Class Brewer to the Database Table)
    - Introduction of the function OnModelCreating to use the BrewerConfiguration when creating the Database.
    - Added a DbSet for the Brewers.

--- BeerhallEF/Data/ApplicationDbContext.cs
+++ BeerhallEF/Data/ApplicationDbContext.cs
@@ -1,14 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BeerhallEF.Data.Mapping;
+using BeerhallEF.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BeerhallEF.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        #region DbSets
+        public DbSet<Brewer> Brewers { get; set; }
+        #endregion
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var connectionstring =
-                             @"Server=.;Database=Beerhall;Integrated Security=True;";
+                                 @"Server=.;Database=Beerhall;Integrated Security=True;";
             optionsBuilder.UseSqlServer(connectionstring);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new BrewerConfiguration());
         }
     }
 }
---
+++ BeerhallEF/Data/Mapping/BrewerConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using BeerhallEF.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BeerhallEF.Data.Mapping
+{
+    class BrewerConfiguration : IEntityTypeConfiguration<Brewer>
+    {
+        public void Configure(EntityTypeBuilder<Brewer> builder)
+        {
+            #region Table 
+            builder.ToTable("Brewer");
+            builder.Property(t => t.BrewerId).ValueGeneratedOnAdd();
+            #endregion
+
+            #region Keys and Indices
+            builder.HasKey(t => t.BrewerId);
+            #endregion
+
+            #region Properties
+            builder.Property(t => t.Name)
+                .HasColumnName("BrewerName")
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(t => t.ContactEmail)
+                .HasMaxLength(100);
+
+            builder.Property(t => t.Street)
+                .HasMaxLength(100);
+            #endregion Properties
+        }
+    }
+}
